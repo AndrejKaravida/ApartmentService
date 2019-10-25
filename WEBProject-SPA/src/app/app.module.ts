7642f0a7e47f8a550a212efca003c33381b7df59
Andrej Karavida <andrejkaravida@gmail.com>
2019-10-25 23:17:18 +0200

    Cliend side login and register Created the navigation and login form Component communication in Angular

--- WEBProject-SPA/src/app/app.module.ts
+++ WEBProject-SPA/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      HomeComponent,
+      NavComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
+      FormsModule,
       HttpClientModule,
       AppRoutingModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
