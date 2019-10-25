9c4e01a07ba2f408b9d22cd473cd6e12e79681bb
Andrej Karavida <andrejkaravida@gmail.com>
2019-10-25 19:47:20 +0200

    Bulding a Walking Skeleton Part 1 Database creation using Entity Framework migrations

--- WEBProject.API/Program.cs
+++ WEBProject.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WEBProjekat.API
+namespace WEBProject.API
 {
     public class Program
     {
